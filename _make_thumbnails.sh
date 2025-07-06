170d7901cd233676aa0c1cfa73093c54e297c94e
rachithp <rachithprakash@gmail.com>
2025-07-05 18:28:28 -0700

    cleanup images

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,11 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+#!/bin/bash
+
+mkdir -p tn/assets
+
+for file in assets/**/*.{jpg,png}; do
+    if [ -f "$file" ]; then
+        output_file="tn/$file"
+        mkdir -p "$(dirname "$output_file")"
+        magick "$file" -thumbnail 160x160 "$output_file"
+    fi
 done
